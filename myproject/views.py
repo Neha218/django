24bfa7cfa10383168c0de3fab5acfd15c30d8eb6
Neha Pawar <nehaapawar21@gmail.com>
2021-12-28 12:35:29 +0530

    HTML Template

--- myproject/views.py
+++ myproject/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('<h3>Hello World</h3>')
+    return render(request, 'home.html')
